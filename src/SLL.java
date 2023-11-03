e50c80f1174c23c8cea819e5fbd8e71ed7d38aec
Velamakanni-Sri-Nandan-Bharadwaj <97480644+Velamakanni-Sri-Nandan-Bharadwaj@users.noreply.github.com>
2023-11-03 12:28:42 -0600

    Update SLL.java
    Added documentation

--- src/SLL.java
+++ src/SLL.java
@@ -1,14 +1,29 @@
 import java.util.*;
-
+/**
+ * A generic class representing a singly-linked list.
+ *
+ * @param <T> The type of elements in the list, which must be comparable.
+ */
 public class SLL<T extends Comparable<T>> {
     private Node<T> head;
     private Node<T> tail;
     private int size = 0;
 
+    /**
+     * Gets the size of the list.
+     *
+     * @return The number of nodes in the list.
+     */
     public int size() {
         return this.size;
     }
-
+    /**
+     * Retrieves the node at a specified index in the list.
+     *
+     * @param i The index of the node to retrieve.
+     * @return The node at the specified index.
+     * @throws IndexOutOfBoundsException if the index is outside the legal bounds of the list.
+     */
     public Node<T> get(int i) {
         if ((i < 0) || (i > (this.size - 1))) {
             throw new IndexOutOfBoundsException("Index i is outside of legal bounds for the singly-linked list.");
@@ -22,6 +37,11 @@ public class SLL<T extends Comparable<T>> {
         return currentNode;
     }
 
+    /**
+     * Adds an element to the beginning of the list.
+     *
+     * @param t The element to add to the list.
+     */
     public void addHead(T t) {
         size++;
 
@@ -40,6 +60,11 @@ public class SLL<T extends Comparable<T>> {
         }
     }
 
+     /**
+     * Adds an element to the end of the list.
+     *
+     * @param t The element to add to the list.
+     */
     public void addTail(T t) {
         size++;
 
@@ -53,7 +78,13 @@ public class SLL<T extends Comparable<T>> {
         }
         tail = tail.getNext();
     }
-
+    
+      /**
+     * Adds an element to the list while maintaining an ordered sequence based on the provided comparator.
+     *
+     * @param n          The node containing the element to add to the list.
+     * @param comparator The comparator used for ordering the elements.
+     */
     public void addInOrder(Node<T> n, Comparator<T> comparator) {
         // Case 1: empty/headless lists.
         if (size == 0) {
